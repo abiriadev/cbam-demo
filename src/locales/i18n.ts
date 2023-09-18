6c079f220cf0a6c1ae8573b253e280abab189dc4
yimeunji <isla@aipim.io>
2023-09-18 13:58:58 +0900

    chore : i18 기본 셋팅

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -1,27 +1,34 @@
-import i18n from 'i18next';
+import i18n, { Resource } from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import translationEn from './translation.en.json';
-import translationKo from './translation.ko.json';
+// 작성된 마크업을 불러옵니다. import 를 간소화하기 위해 *를 사용했습니다.
+import * as en from './en';
+import * as ko from './ko';
 
-const resource = {
+const resources: Resource = {
   en: {
-    translation: translationEn,
+    ...en, // 비구조화 할당을 통해 간소화했습니다.
   },
   ko: {
-    translation: translationKo,
+    ...ko,
   },
-};
+} as const;
 
 i18n.use(initReactI18next).init({
-  resources: resource,
-  lng: 'en',
-  fallbackLng: 'en',
+  resources: resources,
+  lng: 'en', // 초기 설정 언어
+  fallbackLng: {
+    en: ['en'], // 한국어 불러오는 것이 실패했을 경우 영문을 써라 라는 말입니다.
+    default: ['en'],
+  },
 
-  debug: true,
-  keySeparator: '.',
+  debug: false,
+  keySeparator: false,
   interpolation: {
     escapeValue: false,
+  },
+  react: {
+    useSuspense: false,
   },
 });
 
