b27ab0a30666cd6e9bef02cd2456cfd8c3e710e3
Abiria <abiria.dev@gmail.com>
2023-12-13 15:10:34 +0900

    Revert "refactor: move datasource for production process data"
    This reverts commit 2fd0244ee8865cf8e3d44901223bd21e860f34f2.

--- src/pages/summaryProcesses/Tab2.tsx
+++ src/pages/summaryProcesses/Tab2.tsx
@@ -3,7 +3,7 @@ import styles from './SummaryProcesses.module.scss';
 import { Table } from 'antd';
 import { RootState } from 'store';
 import { useSelector } from 'react-redux';
-import { pds, ppds } from './process.ds';
+import { pds } from './process.ds';
 
 const Tab2 = () => {
   const columns1: any = [
@@ -87,7 +87,116 @@ const Tab2 = () => {
                 <Table
                   id="summmaryProcessTB3"
                   size={'small'}
-                  dataSource={pds}
+                  dataSource={[
+                    {
+                      idx: <b>(a)</b>,
+                      d0: <b>Production process</b>,
+                      d1: <b>Aggregated goods category</b>,
+                      d2: <b>Unit</b>,
+                      d3: <b>DirEm*</b>,
+                      d4: (
+                        <b>
+                          Heat
+                          <br /> emissions
+                        </b>
+                      ),
+                      d5: (
+                        <b>
+                          Waste gas <br />
+                          emissions
+                        </b>
+                      ),
+                      d6: (
+                        <b>
+                          Total direct <br />
+                          emissionss
+                        </b>
+                      ),
+                      d7: (
+                        <b>
+                          Indirect, if <br />
+                          relevant
+                        </b>
+                      ),
+                    },
+                    {
+                      idx: 'P1',
+                      d0: 'test',
+                      d1: 'Cement Clinker',
+                      d2: 'tCO2e',
+                      d3: direm,
+                      d4: '0',
+                      d5: '0',
+                      d6: '1,037,310',
+                      d7: '67,952',
+                    },
+                    {
+                      idx: 'P2',
+                      d0: 'test2',
+                      d1: 'Cement',
+                      d2: 'tCO2e',
+                      d3: '0',
+                      d4: '0',
+                      d5: '0',
+                      d6: '0',
+                      d7: '93,537',
+                    },
+                    { idx: 'P3', d2: 'tCO2e' },
+                    { idx: 'P4', d2: 'tCO2e' },
+                    { idx: 'P5', d2: 'tCO2e' },
+
+                    {
+                      d0: <b>Total</b>,
+                      d2: 'tCO2e',
+                      d3: '1,037,310',
+                      d4: '0',
+                      d5: '0',
+                      d6: '1,037,310',
+                      d7: '161,489',
+                    },
+                    {},
+                    {
+                      idx: <b>(b)</b>,
+                      d0: <b>Comparison</b>,
+
+                      d5: <b>Unit</b>,
+                      d6: (
+                        <b>
+                          Total direct
+                          <br />
+                          emissions
+                        </b>
+                      ),
+                      d7: (
+                        <b>
+                          Total indirect
+                          <br />
+                          emissions
+                        </b>
+                      ),
+                    },
+                    {
+                      idx: 'i',
+                      d0: 'Total emissions of the installation (from sheet "C_Emissions&Energy")',
+                      d5: 'tCO2e',
+                      d6: '1,037,310',
+                      d7: '152,370',
+                    },
+                    {
+                      idx: 'ii',
+                      d0: 'Rest (= emissions not attributed to CBAM processes)',
+                      d5: 'tCO2e',
+                      d6: '0',
+                      d7: '-9,119',
+                    },
+                    {
+                      idx: 'iii',
+                      d0: 'Rest (expressed as % of total)',
+                      d5: '-',
+                      d6: '0%',
+                      d7: '-6%',
+                    },
+                  ]}
                   columns={columns1}
                   pagination={false}
                 />
@@ -103,7 +212,52 @@ const Tab2 = () => {
                 <br />
               </ContentBox>
             </div>
-
+            <div style={{ flex: 1 }}>
+              <ContentBox>
+                <div className={styles.ctitle}>
+                  2. Specific direct and indirect (embedded) emissions
+                </div>
+                <br />
+                <div className={styles.sDesc} style={{ width: '940px' }}>
+                  The following abbreviations are used in the tables below:
+                </div>
+                <br />
+                <Table
+                  id="summmaryProcessTB4"
+                  size={'small'}
+                  dataSource={[
+                    {
+                      d0: 'SE (direct)',
+                      d1: 'Specific direct emissions of the production process, i.e. excluding any embedded emissions from any precursors consumed in the process.								',
+                    },
+                    {
+                      d0: 'SEE (direct)',
+                      d1: 'Specific embedded direct emissions of the production process, i.e. including any embedded emissions from any precursors consumed in the process.								',
+                    },
+                    {
+                      d0: 'SE (indirect)',
+                      d1: 'Specific indirect emissions of the production process, i.e. excluding any embedded indirect emissions from any precursors consumed in the process.								',
+                    },
+                    {
+                      d0: 'SEE (indirect)',
+                      d1: 'Specific embedded indirect emissions of the production process, i.e. including any embedded indirect emissions from any precursors consumed in the process.								',
+                    },
+                    {
+                      d0: 'Electricity consumed',
+                      d1: 'Specific electricity consumption within the production process.								',
+                    },
+                    {
+                      d0: 'Embedded electricity',
+                      d1: 'Specific embedded electricity consumption of the production process, i.e. including any embedded electricity consumption in other production processes within the installation (i.e. excl. from purchased precursors).								',
+                    },
+                    {
+                      d0: 'CP due (per t or MWh)',
+                      d1: 'This information is taken from the "tool to calculate the carbon price due" in sheet "F_Tools", where relevant.								',
+                    },
+                    {
+                      d0: 'Rebate (per t or MWh)',
+                      d1: 'This information is taken from the "tool to calculate the carbon price due" in sheet "F_Tools", where relevant.								',
+                    },
                   ]}
                   columns={[
                     {
@@ -121,7 +275,7 @@ const Tab2 = () => {
                 <Table
                   id="summmaryProcessTB5"
                   size={'small'}
-                  dataSource={ppds}
+                  dataSource={pds}
                   columns={[
                     {
                       title: '',
